9a99a065d90f0ba3caa8cd66e3aad803c09a0cfe
Knorrke <knorr.b@gmx.de>
2017-06-27 15:48:50 +0200

    configure port for heroku

--- src/main/java/config/WebConfig.java
+++ src/main/java/config/WebConfig.java
@@ -9,7 +9,16 @@ import modules.user.service.UserService;
 public class WebConfig {
 	public WebConfig(PostService postService, UserService userService) {
     	staticFileLocation("/public");
-    	port(30003);
+        port(getHerokuAssignedPort());
+
     	new Router(postService, userService).setupRoutes();
+    }
+	
+	static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 30003; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
