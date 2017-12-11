255d28207ab770a40dc33b04b23808331bdf8bed
Knorrke <knorr.b@gmx.de>
2017-12-11 19:21:50 +0100

    configure Database for heroku

--- src/main/java/config/DatabaseConfig.java
+++ src/main/java/config/DatabaseConfig.java
@@ -1,32 +1,51 @@
 package config;
 
-import java.sql.SQLException;
-
 import javax.sql.DataSource;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.jdbc.datasource.init.ScriptException;
-import org.springframework.jdbc.datasource.init.ScriptUtils;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 @Configuration
 public class DatabaseConfig {
 
 	@Bean
 	public DataSource dataSource() throws Exception {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("org.hsqldb.jdbcDriver");
-        dataSource.setUrl("jdbc:hsqldb:file:src/main/resources/database/database");
-        dataSource.setUsername("SA");
-        dataSource.setPassword("");
+		if (System.getenv("JDBC_DATABASE_URL") != null) {
+			String dbUrl = System.getenv("JDBC_DATABASE_URL");
+	        String username = System.getenv("JDBC_DATABASE_USERNAME");
+	        String password = System.getenv("JDBC_DATABASE_PASSWORD");
+
+	        BasicDataSource basicDataSource = new BasicDataSource();
+	        basicDataSource.setDriverClassName("org.postgresql.Driver");
+	        basicDataSource.setUrl(dbUrl);
+	        basicDataSource.setUsername(username);
+	        basicDataSource.setPassword(password);
+	
+	        return basicDataSource;
+		} else {
+			EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+			EmbeddedDatabase db = builder
+					.setType(EmbeddedDatabaseType.HSQL)
+					.addScript("sql/create-db.sql")
+					.addScript("sql/insert-data.sql")
+					.build();
+			return db;
+		}
+		
+//		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//		dataSource.setDriverClassName("org.hsqldb.jdbcDriver");
+//        dataSource.setUrl("jdbc:hsqldb:file:src/main/resources/database/database");
+//        dataSource.setUsername("SA");
+//        dataSource.setPassword("");
 /*        Resource create = new ClassPathResource("sql/create-db.sql");
         Resource insert = new ClassPathResource("sql/insert-data.sql");
         ScriptUtils.executeSqlScript(dataSource.getConnection(), create);
         ScriptUtils.executeSqlScript(dataSource.getConnection(), insert);*/
-        return dataSource;
+//        return dataSource;
 	}
 
 }
