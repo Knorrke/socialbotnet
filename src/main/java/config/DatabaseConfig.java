2c75eb90ac6e0f30f323337ae6e8662d139af2e3
Knorrke <knorr.b@gmx.de>
2019-03-31 21:16:22 +0200

    feat: allow persistent database

--- src/main/java/config/DatabaseConfig.java
+++ src/main/java/config/DatabaseConfig.java
@@ -1,15 +1,28 @@
 package config;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import javax.sql.DataSource;
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
 
 @Configuration
 public class DatabaseConfig {
+  /** Set to true to use in-memory database * */
+  public static boolean DEBUG_MODE = false;
+
+  static final Logger logger = LoggerFactory.getLogger(DatabaseConfig.class);
 
   @Bean
   public DataSource dataSource() throws Exception {
@@ -24,27 +37,51 @@ public class DatabaseConfig {
       basicDataSource.setUsername(username);
       basicDataSource.setPassword(password);
 
+      createSchemeIfNotExists(basicDataSource, "sql/create-db-postgresql.sql");
       return basicDataSource;
     } else {
-      EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-      EmbeddedDatabase db =
-          builder
-              .setType(EmbeddedDatabaseType.HSQL)
-              .addScript("sql/create-db.sql")
-              .addScript("sql/insert-data.sql")
-              .build();
-      return db;
+      if (DEBUG_MODE) {
+        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        EmbeddedDatabase db =
+            builder
+                .setType(EmbeddedDatabaseType.HSQL)
+                .addScript("sql/create-db.sql")
+                .addScript("sql/insert-data.sql")
+                .build();
+        return db;
+      } else {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.hsqldb.jdbcDriver");
+        dataSource.setUrl("jdbc:hsqldb:file:database/database");
+        dataSource.setUsername("SA");
+        dataSource.setPassword("");
+        createSchemeIfNotExists(dataSource, "sql/create-db.sql");
+        return dataSource;
+      }
     }
+  }
 
-    //		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-    //		dataSource.setDriverClassName("org.hsqldb.jdbcDriver");
-    //        dataSource.setUrl("jdbc:hsqldb:file:src/main/resources/database/database");
-    //        dataSource.setUsername("SA");
-    //        dataSource.setPassword("");
-    /*        Resource create = new ClassPathResource("sql/create-db.sql");
-    Resource insert = new ClassPathResource("sql/insert-data.sql");
-    ScriptUtils.executeSqlScript(dataSource.getConnection(), create);
-    ScriptUtils.executeSqlScript(dataSource.getConnection(), insert);*/
-    //        return dataSource;
+  private void createSchemeIfNotExists(DataSource dataSource, String sqlScriptPath)
+      throws SQLException {
+    try (Connection connection = dataSource.getConnection()) {
+      // check if table post exists
+      ResultSet rs = connection.getMetaData().getTables(null, null, "%", new String[] {"TABLE"});
+      boolean initialized = false;
+      while (rs.next() && !initialized) {
+        if (rs.getString("TABLE_NAME").equalsIgnoreCase("post")) {
+          logger.info("Loading existing database");
+          initialized = true;
+        }
+      }
+      if (!initialized) {
+        logger.info("Initializing database");
+        Resource create = new ClassPathResource(sqlScriptPath);
+        ScriptUtils.executeSqlScript(connection, create);
+      }
+    }
+  }
+
+  public static void setDebugMode(boolean debug) {
+    DEBUG_MODE = debug;
   }
 }
