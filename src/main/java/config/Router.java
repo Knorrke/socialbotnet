fc8a9896413bdd14c5f406829a3e4df4d605d273
Knorrke <knorr.b@gmx.de>
2020-03-15 14:26:53 +0100

    activate CORS for api routes

--- src/main/java/config/Router.java
+++ src/main/java/config/Router.java
@@ -72,6 +72,28 @@ public class Router {
         "/api",
         () -> {
           before("/*", (req, res) -> logger.info("Received api call to " + req.pathInfo()));
+
+          // Activate CORS
+          options(
+              "/*",
+              (req, res) -> {
+                String accessControlRequestHeaders = req.headers("Access-Control-Request-Headers");
+                if (accessControlRequestHeaders != null) {
+                  res.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+                }
+
+                String accessControlRequestMethod = req.headers("Access-Control-Request-Method");
+                if (accessControlRequestMethod != null) {
+                  res.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+                }
+                return "OK";
+              });
+          before(
+              "/*",
+              (req, res) -> {
+                res.header("Access-Control-Allow-Origin", "*");
+                res.header("Access-Control-Allow-Headers", "*");
+              });
           before("/*", this::checkCorrectRequestType);
           before(
               "/*",
